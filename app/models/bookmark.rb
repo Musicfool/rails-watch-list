eff82289e6d6ca77c2ad7c89b7540e69c346d9fd
Mathieu Rabet <rabet.mathieu@gmail.com>
2024-02-22 17:02:51 +0100

    incomplete watchlist
    Signed-off-by: Mathieu Rabet <rabet.mathieu@gmail.com>

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :comment, length: { minimum: 6 }
+  validates :comment, length: { minimum: 6 }, presence: true
   validates_uniqueness_of :movie_id, scope: :list_id, message: "A movie can only have one list!"
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,14 +1,6 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  has_many :lists, through: :bookmarks
 
-  before_destroy :check
-
-  def check
-    false
-  end
-
-  validates_associated :bookmarks
   validates :title, :overview, presence: true
   validates :title, uniqueness: true
 end
